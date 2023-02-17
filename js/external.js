28a72e8fd342f047e38b8f2f1dd9dfe8f9e6bf24
emirmartinez <emirmartinez@gmail.com>
2023-02-17 13:29:51 -0600

    Added html and js file.

---
+++ js/external.js
@@ -0,0 +1,7 @@
+console.log("Hello from external JavaScript");
+
+alert("Welcome to my Website!");
+
+let favColor = prompt("What is your favorite color?");
+
+alert(`Great, ${favColor} is my favorite color too!`)
